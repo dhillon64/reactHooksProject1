0e7106e31caba0a1dd362aba587da1362a6bca87
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-08-13 23:08:52 +0100

    added dropdown component, passed selection to app, which changed selection state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import {Accordion} from './Components/Accordion';
-import Search from './Components/Search';
-const items =[
-    {
-        title:'What is React?',
-        content:'React is a front end javascript framework'
-    },
-    {
-        title:'Why use React',
-        content: 'React is a favourite JS library among engineers'
-    },
-    {
-        title:'How do you use React?',
-        content:'You use React by creating components'
-    },
+import React, { useState } from "react";
+import { Accordion } from "./Components/Accordion";
+import Search from "./Components/Search";
+import Dropdown from "./Components/Dropdown";
+
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React",
+    content: "React is a favourite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
 ];
 
+const options = [
+  {
+    label: "The color Red",
+    value: "red",
+  },
+  {
+    label: "The color Green",
+    value: "green",
+  },
+  {
+    label: "A shade of Blue",
+    value: "blue",
+  },
+];
 
-export default () =>{
-    return ( 
+const App = () => {
+  const [selection, setSelected] = useState(options[0]);
+
+  return (
     <div>
-        <Search />
+      <Dropdown
+        selected={selection}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
-    );
+  );
 };
+
+export default App;
