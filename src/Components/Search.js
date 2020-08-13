1f72e466a81242d5e1e8956af0a8ebc9834356fb
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-08-13 21:05:50 +0100

    finished working on Search bar widget

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,27 +1,75 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-const Search=()=>{
+const Search = () => {
+  const [term, changeTerm] = useState("ps5");
+  const [results, setResults] = useState([]);
 
-    const [term,changeTerm]=useState('');
+  console.log(results);
 
-    useEffect(()=>{
-        console.log('I Run after every render and at initial render');
-    },[term])
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
 
-    const handleChange=e=>{
-        changeTerm(e.target.value);
+    if (term && !results.length) {
+      search();
+    } else {
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 1000);
+
+      return () => {
+        clearTimeout(timeoutId);
+      };
     }
+  }, [term]);
 
+  const searchResults = results.map((result) => {
     return (
-        <div>
-            <div className="ui form">
-                <div className="field">
-                    <label>Enter Search Term</label>
-                    <input type="text" onChange={handleChange}/>
-                </div>
-            </div>
+      <div key={result.pageid} className="item">
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            Go
+          </a>
         </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
     );
+  });
+
+  const handleChange = (e) => {
+    changeTerm(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input value={term} type="text" onChange={handleChange} />
+        </div>
+      </div>
+      <div className="ui celled list">{searchResults}</div>
+    </div>
+  );
 };
 
 export default Search;
